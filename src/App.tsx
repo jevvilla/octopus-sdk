5debc58755437619eada6d4fc16cbb696e83fc4a
Esteban Villa <esteban.villa@EVilla-MBP-M1.local>
2023-07-11 06:35:55 -0500

    version v0.0.1 update 1

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Version {pkg?.version}</h1> change
+      <h1>Vite + React Version {pkg?.version}</h1> NEW VERSION TESTING API
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
